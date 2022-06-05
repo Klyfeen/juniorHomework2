82f09d2aa9a9e940a1db7717f34f0c6dcc2cad69
Klyfeen <mlgggeasy@gmail.com>
2022-06-05 17:38:56 +0500

    Add project files.

---
+++ juniorHomework2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace juniorHomework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string favoriteGame = "Bloodborne";
+            int bulletsForWeapon = 150;
+            bool isWaterCold = true;
+            char mapTag = '@';
+            float height = 193.7f;
+            byte enemyDamage = 130;
+            long distanceToTheMoon = 384400;
+            short coinsForShopping = 25300;
+            ulong unitedStatesExternalDebt = 18446744073709551615;
+            sbyte zombies = 100;
+        }
+    }
+}
